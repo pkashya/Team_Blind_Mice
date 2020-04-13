f920089c6ed85fc3ca61689c2cf7c8d5537d2dff
dq090125 <54640069+dq090125@users.noreply.github.com>
2020-04-13 17:47:50 -0400

    New audio parsing code

---
+++ Qi Dai/Codes/Parse.py
@@ -0,0 +1,26 @@
+import re
+from gtts import gTTS
+filename = 'Sample.txt'
+file = open(filename, 'r')
+text = file.read()
+file.close()
+lst = re.split(',|\*',text)
+i = 1
+with open("Sorted.txt",'w') as file:
+    for st in lst:
+        file.write(st+'\n')
+        mytext = st
+        language = 'en'
+        myobj = gTTS(text=mytext, lang=language, slow=False)
+        myobj.save('%s.mp3' % i)
+        print('File save!')
+        i+=1
+file.close()
+
+
+
+
+
+
+
+
